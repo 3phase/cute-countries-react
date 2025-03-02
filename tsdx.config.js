c01e04220daba08ae19ba313147ba7555a97da32
3phase <pbozhinow@gmail.com>
2025-03-02 21:45:36 +0100

    Bumps new versions of the environment and moves the example app away of the project

--- tsdx.config.js
+++ tsdx.config.js
@@ -1,10 +1,14 @@
 const postcss = require('rollup-plugin-postcss');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
+const copy = require('rollup-plugin-copy');
+const image = require('@rollup/plugin-image');
+
 
 module.exports = {
   rollup(config, options) {
     config.plugins.push(
+      image(),
       postcss({
         plugins: [
           autoprefixer(),
@@ -15,7 +19,12 @@ module.exports = {
         inject: false,
         // only write out CSS for the first bundle (avoids pointless extra files):
         extract: !!options.writeMeta,
-      })
+      }),
+      copy({
+        targets: [
+          { src: 'src/public/**/*', dest: 'dist/public/.' }
+        ]
+      }),
     );
     return config;
   },
