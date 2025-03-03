9f2520d2a2789551274130b4be9f1071b6a7e080
3phase <pbozhinow@gmail.com>
2025-03-03 11:36:54 +0100

    Removes support for Variants

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,11 @@ import React, { HTMLAttributes, JSX } from 'react';
 import { FlagName, getFlag } from './svgs';
 import './styles.css';
 
-export type Variants = undefined | 'rounded' | 'square' | 'circle';
-
 export interface FlagProps extends HTMLAttributes<HTMLImageElement> {
   country?: FlagName;
   role?: string;
   size?: number;
   alt?: string;
-  variant?: Variants;
   className?: string;
 }
 
@@ -17,7 +14,6 @@ const Flag = ({
   size = 24,
   alt,
   className,
-  // variant,
   country = 'de',
 }: FlagProps): JSX.Element => {
   var test = React.createElement('div', {
@@ -26,7 +22,6 @@ const Flag = ({
     className: `icon-inline ${className}`,
     style: {
       width: !!size ? `${size}px` : '24px',
-      // (variant && {...styleContainer[variant!] as any}),
     },
     dangerouslySetInnerHTML: { __html: getFlag(country) },
   });
